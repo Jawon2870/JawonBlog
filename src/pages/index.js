90bc94cb54784f273af25efcd52027fc113688de
Jawon <2870881842@qq.com>
2025-01-08 15:07:58 +0800

    '1'

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to={`${siteConfig.themeConfig.startPath}`}>
+            to={siteConfig.themeConfig.startPath}>
             开始阅读
           </Link>
         </div>
