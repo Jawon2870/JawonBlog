3c3529d7addf92eab2c175ffa1c74406386d61e0
Jawon <2870881842@qq.com>
2025-01-24 17:00:27 +0800

    '1'

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.get('*', (req, res) => {
+    console.log(req.url);
+    res.send("??? what you want to see here?");
+});
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running at: http://localhost:${port}`);
+});
