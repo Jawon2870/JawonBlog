17f02fc9e8607c309f28896b615ed158c75b9b2c
Jawon <2870881842@qq.com>
2025-02-05 17:34:13 +0800

    '1'

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const hostname = 'https://www.jawon.site';
 
 /**
- * 将目标目录作为静态资源提供给客户端，如果请求路径属于该目录，
- * 则直接返回请求的资源，否则将继续向下执行，检查是否存在对应的路由。
+ * 设置作为静态资源响应的目录，如果请求路径指向静态资源，
+ * 则返回静态资源，否则继续向下寻找对应的路由。
 */
 app.use(express.static(path.join(__dirname, './Blog/build')));
 
+// 对于其他请求，自定义响应结果
 app.get('*', (req, res) => {
-    console.log('received URL: ' + hostname + req.url);
-    
-    // res.sendFile(path.join(__dirname, './Blog/build', 'index.html'));
-
-    res.send('<h1 style="text-align:center">404 Not Found</h1>');
+    console.log('received URL: ' + req.url);
+    res.sendFile(path.join(__dirname, './Blog/build/index.html'));
+    // res.send('<h1 style="text-align:center">404 Not Found</h1>');
 });
 
 let port = process.env.PORT || 3000;
