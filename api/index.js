4839fa23b75a3bf0a34f5c0450ce484c585fa17f
Jawon <2870881842@qq.com>
2025-02-05 14:18:16 +0800

    '1'

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const hostname = 'https://www.jawon.site';
  * 将目标目录作为静态资源提供给客户端，如果请求路径属于该目录，
  * 则直接返回请求的资源，否则将继续向下执行，检查是否存在对应的路由。
 */
-app.use(express.static(path.join(__dirname, './Blog/build')));
+// app.use(express.static(path.join(__dirname, './Blog/build')));
 
 app.get('*', (req, res) => {
     console.log('received URL: ' + hostname + req.url);
